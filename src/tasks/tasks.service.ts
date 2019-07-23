c5f45ba2ba14f112fc80482fc83296ee77906ab4
hongphuc2197 <hongphuc2197@gmail.com>
2019-07-23 17:57:25 +0700

    first commit

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
+import * as uuid from 'uuid/v1';
+import { CreateTaskDto } from './dto/create-task.dto';
+
+@Injectable()
+export class TasksService {
+  private tasks: Task[] = [];
+
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
+  getTaskById(id: string): Task {
+    return this.tasks.find(tasks => tasks.id === id);
+  }
+  deleteTask(id: string): void {
+    this.tasks = this.tasks.filter(tasks => tasks.id !== id);
+  }
+  updateTaskStatus(id: string, status: TaskStatus): Task {
+    const task = this.getTaskById(id);
+    task.status = status;
+    return task;
+  }
+
+  createTask(createTaskDto: CreateTaskDto): Task {
+    const { title, description } = createTaskDto;
+    const task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+    this.tasks.push(task);
+    return task;
+  }
+}
